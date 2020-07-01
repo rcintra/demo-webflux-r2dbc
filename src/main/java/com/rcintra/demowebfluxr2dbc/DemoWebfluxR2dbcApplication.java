3b8378bb7428586a0da73608d249aef3013cdcca
Rafael Cintra <ra.cintra@gmail.com>
2020-07-01 14:47:35 -0300

    first commit

---
+++ src/main/java/com/rcintra/demowebfluxr2dbc/DemoWebfluxR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.rcintra.demowebfluxr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebfluxR2dbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebfluxR2dbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcintra/demowebfluxr2dbc/DemoWebfluxR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcintra.demowebfluxr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebfluxR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
